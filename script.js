6c3cfafbfbed2a18f72a45eced71e5bfaf44ec14
Afiansyah Cahyo Wicaksono <50729239+alfinxd4@users.noreply.github.com>
2024-04-23 23:29:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector(".navbar-toogler");
+ const btn = document.querySelector(".navbar-toogler");
 const menu = document.querySelectorAll(".nav-item");
 
 menu.forEach(item => {
@@ -12,3 +12,34 @@ menu.forEach(item => {
     })
 
 });
+
+// reveal animation
+const reveal = () => {
+    const reveals = document.querySelectorAll(".reveal");
+    const revealstop = document.querySelectorAll(".reveal-top");
+
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 85;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+
+    for (let i = 0; i < revealstop.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = revealstop[i].getBoundingClientRect().top;
+        const elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            revealstop[i].classList.add("active");
+        } else {
+            revealstop[i].classList.remove("active");
+        }
+    }
+};
+window.addEventListener("scroll", reveal);
