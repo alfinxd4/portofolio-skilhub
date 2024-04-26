4b2994ca61245a93a4d41bd00d8cb09b93a0bcb6
Afiansyah Cahyo Wicaksono <50729239+alfinxd4@users.noreply.github.com>
2024-04-27 00:23:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,60 @@
 const btn = document.querySelector(".navbar-toogler");
 const menu = document.querySelectorAll(".nav-item");
-
 const hamburger = document.querySelector(".menu");
 
+// menu animation
+menu.forEach((item) => {
+  btn.addEventListener("click", () => {
+    if (item.style.display == "none") {
+      item.style.display = "block";
+      hamburger.classList.remove("fa-bars");
+      hamburger.classList.add("fa-xmark");
 
-menu.forEach(item => {
-
-    btn.addEventListener('click', () => {
-        if (item.style.display == 'none') {
-            item.style.display = 'block';
-            hamburger.classList.remove("fa-bars");
-            hamburger.classList.add("fa-xmark");
-            
-    item.addEventListener('click',()=>{
-        let clicked=true;
-        if (clicked==true) {
-            menu.forEach(item=>{
-                    item.style.display = 'none';
+      item.addEventListener("click", () => {
+        let clicked = true;
+        if (clicked == true) {
+          menu.forEach((item) => {
+            item.style.display = "none";
             hamburger.classList.remove("fa-xmark");
             hamburger.classList.add("fa-bars");
-            })
+          });
         }
-    
-    })
-               
-        } else {
-            item.style.display = 'none';
-            hamburger.classList.remove("fa-xmark");
-            hamburger.classList.add("fa-bars");
-        }
-    })
+      });
+    } else {
+      item.style.display = "none";
+      hamburger.classList.remove("fa-xmark");
+      hamburger.classList.add("fa-bars");
+    }
+  });
 });
 
- 
 // reveal animation
 const reveal = () => {
-    const reveals = document.querySelectorAll(".reveal");
-    const revealstop = document.querySelectorAll(".reveal-top");
+  const reveals = document.querySelectorAll(".reveal");
+  const revealstop = document.querySelectorAll(".reveal-top");
 
-    for (let i = 0; i < reveals.length; i++) {
-        const windowHeight = window.innerHeight;
-        const elementTop = reveals[i].getBoundingClientRect().top;
-        const elementVisible = 85;
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 85;
 
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
+  }
 
-    for (let i = 0; i < revealstop.length; i++) {
-        const windowHeight = window.innerHeight;
-        const elementTop = revealstop[i].getBoundingClientRect().top;
-        const elementVisible = 100;
+  for (let i = 0; i < revealstop.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = revealstop[i].getBoundingClientRect().top;
+    const elementVisible = 100;
 
-        if (elementTop < windowHeight - elementVisible) {
-            revealstop[i].classList.add("active");
-        } else {
-            revealstop[i].classList.remove("active");
-        }
+    if (elementTop < windowHeight - elementVisible) {
+      revealstop[i].classList.add("active");
+    } else {
+      revealstop[i].classList.remove("active");
     }
+  }
 };
 window.addEventListener("scroll", reveal);
